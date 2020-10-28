3a64f4e518558740b42f9537556bdb8bfd9c3a66
Simon Jenner <sjenner@kth.se>
2020-10-28 18:31:30 +0100

    Sista dagen

--- Lektioner/Bank/Main.java
+++ Lektioner/Bank/Main.java
@@ -1,9 +1,49 @@
+package com.company;
+
 import java.util.Scanner;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        Bank bank = new Bank(3);
-        Person person = new Person("Simon", "Password");
-        System.out.println(person.viewName());
+        Meny();
+    }
+
+    public static void Meny() {
+        Bank bank = new Bank(5);
+        Scanner sc;
+        int choice;
+        while (true) {
+            sc = new Scanner(System.in);
+            System.out.println("1. Add person\n2. Add money\n3. Exit");
+            choice = sc.nextInt();
+            switch (choice) {
+                case (1):
+                    bank.addPerson();
+                    break;
+                case (2):
+                    sc.nextLine();
+                    System.out.println("Vad är ditt namn?");
+                    String name = sc.nextLine();
+                    System.out.println(name);
+                    System.out.println("Vad är ditt lösenord?");
+                    String password = sc.nextLine();
+                    System.out.println(password);
+                    System.out.println("Hur mycket vill du sätta in?");
+                    double money = sc.nextDouble();
+                    System.out.println(money);
+                    sc.nextLine();
+
+                    try{
+                        bank.addMoney(name, password, money);
+                    }catch (Exception e){
+                        System.out.println("Fanns inte");
+                    }
+                    break;
+                case (3):
+                    System.exit(0);
+                    break;
+             }
+        }
     }
 }
 
@@ -12,59 +52,62 @@ class Person{
     private double money;
     private String password;
 
-    Person(String name, double money, String password){
+    Person(String name, double money, String password) {
         this.name = name;
         this.money = money;
         this.password = password;
     }
 
-    Person(String name, String password){
+    Person(String name, String password) {
         this.name = name;
         this.money = 0;
         this.password = password;
     }
 
-    public void changePassword(String newpassword){
+    public void changePassword(String newpassword) {
         this.password = newpassword;
     }
 
-    public String viewPassword(){
+    public String viewPassword() {
         return this.password;
     }
 
-    public String viewName(){
+    public String viewName() {
         return this.name;
     }
 
-    public double viewMoney(){
+    public double viewMoney() {
         return this.money;
+    }
+
+    public void changeMoney(double money) {
+        this.money = money;
     }
 }
 
-class Bank{
+class Bank {
     int nrOfPeople;
     Person[] people;
 
-    Bank(){
+    Bank() {
         nrOfPeople = 0;
         people = new Person[10];
     }
 
-    Bank(int arraySize){
+    Bank(int arraySize) {
         nrOfPeople = 0;
         people = new Person[arraySize];
     }
 
-    public void addPerson(){
+    public void addPerson() {
         String password;
         String second;
         Scanner sc = new Scanner(System.in);
         System.out.println("Vad heter du?");
         String name = sc.nextLine();
-
         int l = 1;
-        do{
-            if(l!=1){
+        do {
+            if (l != 1) {
                 System.out.println("De var inte samma. Välj igen!");
             }
             System.out.println("Välj ett lösenord!");
@@ -72,29 +115,31 @@ class Bank{
             System.out.println("Bekräfta lösenord!");
             second = sc.nextLine();
             l++;
-        }while(!password.equals(second));
+        } while (!password.equals(second));
 
-        people[nrOfPeople] = new Person(name, password);
-
+        this.people[nrOfPeople] = new Person(name, password);
         System.out.println("Kontot har skapats");
         nrOfPeople++;
     }
 
-    public void viewPeople(){
-        for(int i=0;i<people.length; i++){
+    public void viewPeople() {
+        for (int i = 0; i < people.length; i++) {
             System.out.println(people[i].viewName());
         }
     }
 
-    public void addMoney(String name, String password, double money){
-        Scanner sc = new Scanner(System.in);
-        for(Person p : people){
-            if(p.viewName().equals(name)){
-                if(p.viewPassword().equals(password)){
-
+    public void addMoney(String name, String password, double money) {
+        for (Person p : people) {
+            if (p.viewName().equals(name)) {
+                if (p.viewPassword().equals(password)) {
+                    p.changeMoney(money);
+                    System.out.println("Ditt nya saldo är: " + p.viewMoney());
+                } else {
+                    System.out.println("Fel lösenord");
                 }
+                return;
             }
         }
-
+        System.out.println("Namnet finns inte");
     }
 }
